aa91fa23c2b8d5a4c8a1cbb5a71a3248e334c940
Kiril Vangelovski <kiril@hacbee.com>
2015-03-27 12:47:11 +0100

    [INIT]

---
+++ l10n_mk/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# This file is part of OpenERP. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
+{
+    'name': 'Macedonian localization',
+    'version': '0.1',
+    'author': 'HacBee UAB',
+    'category': 'Custom',
+    'website': 'http://www.hbee.eu',
+    'summary': '',
+    'description': """
+Macedonian localization
+""",
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
