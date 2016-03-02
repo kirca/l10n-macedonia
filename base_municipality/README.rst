74f7d4292faf4485be159b97f992f15a1da8a3fc
Kiril Vangelovski <kiril@hbee.eu>
2016-03-02 12:57:58 +0100

    [IMP] base_municipality, i18n_mk #4139

---
+++ base_municipality/README.rst
@@ -0,0 +1,6 @@
+**Municipalities in R. Macedonia**
+
+Description what the add-on does:
+
+Adds zip code on res.country.state;
+Imports all municipalities from R. Macedonia;
---
+++ i18n_mk/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# This file is part of Odoo. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
+{
+    'name': 'Translations for R. Macedonia',
+    'version': '8.0.1.0',
+    'author': 'HBEE',
+    'category': 'Localization',
+    'website': 'https://hbee.eu/',
+    'licence': 'AGPL-3',
+    'summary': 'Overwrites offical Odoo translations',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': False,
+}
